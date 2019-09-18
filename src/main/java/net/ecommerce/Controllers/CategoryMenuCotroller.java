a879b2a4f5faf5542032b49ca9877e22363b159d
Mastro <mastrositumorang1@gmail.com>
2019-09-18 11:09:46 +0700

    Update Unit Test for Controllers and Repositories

--- src/main/java/net/ecommerce/Controllers/CategoryMenuCotroller.java
+++ src/main/java/net/ecommerce/Controllers/CategoryMenuCotroller.java
@@ -1,19 +1,32 @@
 package net.ecommerce.Controllers;
 
+import net.ecommerce.Apps.AppsApplicationTests;
 import net.ecommerce.Models.CategoryMenu;
 import net.ecommerce.Repositories.CategoryMenuRepository;
 import net.ecommerce.Repositories.UserRepository;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.bind.annotation.*;
+
+import com.nimbusds.openid.connect.sdk.Prompt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.Produces;
+
 @RestController
 @RequestMapping("category/")
-public class CategoryMenuCotroller {
+public class CategoryMenuCotroller extends AppsApplicationTests{
 
     @Autowired
     private CategoryMenuRepository categoryMenuRepository;
@@ -22,6 +35,7 @@ public class CategoryMenuCotroller {
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
 	@GetMapping(path = "/get-all/{user_id}", produces="application/json")
+    @Test
     public @ResponseBody
     Map getAllCategoryForUserA(@PathVariable("user_id") String userId)
     {
@@ -40,4 +54,16 @@ public class CategoryMenuCotroller {
         returnData.put("modules", allCategory);
         return returnData;
     }
+    	@Test
+    	public void getAllCategoryForUserA() throws Exception {
+	      String uri = "/user";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      String content = mvcResult.getResponse().getContentAsString();
+	      Produces[] userlist = (Produces[]) super.mapFromJson(content, Prompt[].class);
+	      assertTrue(userlist.length > 0);
+	   }
 }
--- src/test/java/net/ecommerce/Apps/AppsApplicationTests.java
+++ src/test/java/net/ecommerce/Apps/AppsApplicationTests.java
@@ -1,16 +1,59 @@
 package net.ecommerce.Apps;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.google.gson.JsonParseException;
+
+import net.ecommerce.Models.User;
+import net.ecommerce.Repositories.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class AppsApplicationTests {
+	   protected MockMvc mvc;
+	   @Autowired
+	   WebApplicationContext webApplicationContext;
+
+	   protected void setUp() {
+	      mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	   }
+	   protected String mapToJson(Object obj) throws IOException {
+	      ObjectMapper objectMapper = new ObjectMapper();
+	      return objectMapper.writeValueAsString(obj);
+	   }
+	   protected <T> T mapFromJson(String json, Class<T> clazz)
+	      throws JsonParseException, JsonMappingException, IOException {
+	      
+	      ObjectMapper objectMapper = new ObjectMapper();
+	      return objectMapper.readValue(json, clazz);
+	   }
 
 	@Test
 	public void contextLoads() {
 	}
+	
+	  @Autowired
+	  private UserRepository userRepository;
 
+	  @Test
+	  public void saveTest() {
+	    User user = new User("Tono", 0, null, null, null, false, null, null, null, null);
+	    userRepository.save(user);
+	    Assert.assertNotNull(userRepository.findById("Tono"));
+	  }
+	  
 }
